5608d82207231e8ca8fee2e8bfa177a501ea4f56
lautaro <kautarol@gmail.com>
2021-12-28 20:56:03 -0300

    deploy 11

--- api/src/app.js
+++ api/src/app.js
@@ -13,7 +13,7 @@ server.name = 'API';
 
 server.use(
   cors({
-    origin: ["http://localhost:3000", "https://pi-countries-henry.vercel.app"],
+    origin: "https://pi-countries-henry.vercel.app",
     credentials: true,
   })
 );
@@ -22,7 +22,7 @@ server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use(morgan('dev'));
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Origin', 'https://pi-countries-henry.vercel.app'); // update to match the domain you will make the request from
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
